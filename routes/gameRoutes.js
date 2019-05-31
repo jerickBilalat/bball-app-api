b111fce8d9ba571bea8aa41b865c097c8f052b1d
Jerick Bilalat <jrk.mb.xm@gmail.com>
2019-05-31 15:41:07 -0500

    add update players stats fucntionality when creating games

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const GameModel = require('../models/gameModel')
+const PlayerModel = require('../models/playerModel')
 
 router.get('/', getAllGames)
 router.post('/create_game', createGame)
@@ -17,6 +18,26 @@ function getAllGames(req, res, next) {
 }
 
 function createGame(req, res, next) {
+  const {winners, losers} = req.body
+
+  // update player stats
+  winners.forEach( playerId => {
+    PlayerModel
+      .findByIdAndUpdate(playerId, { $inc: {wins: 1} })
+      .exec((err) => {
+        if(err) return next(err);
+      })
+  });
+
+  losers.forEach( playerId => {
+    PlayerModel
+      .findByIdAndUpdate(playerId, { $inc: {losses: 1} })
+      .exec((err) => {
+        if(err) return next(err);
+      })
+  })
+  
+  // create game
   GameModel
     .create(req.body, (err, game) => {
       if(err) return next(err)
